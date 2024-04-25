6cb74c5e941be69a54d92837c751aee50d40da8d
Saranyasakthi-4 <saranyasakthi.moha@gmail.com>
2024-04-25 00:15:11 -0400

    Initial commit and framework

---
+++ LMS_seleniumPhase2/src/test/java/com/LMS/pageobjects/DashBoardPage.java
@@ -0,0 +1,13 @@
+package com.LMS.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class DashBoardPage {
+	 private WebDriver webDriver;
+	
+    public DashBoardPage(WebDriver webDriver) {
+        super();
+        this.webDriver = webDriver;
+    }
+
+}
---
+++ LMS_seleniumPhase2/src/test/java/com/LMS/stepdefenition/HomePageValidation_SD.java
@@ -0,0 +1,84 @@
+package com.LMS.stepdefenition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HomePageValidation_SD {
+	@Given("Admin launch the browser_HomePage")
+	public void admin_launch_the_browser_home_page() {
+	}
+
+	@When("Admin gives the correct LMS portal URL_HomePage")
+	public void admin_gives_the_correct_lms_portal_url_home_page() {
+	}
+
+	@Then("Admin should land on the home page_HomePage")
+	public void admin_should_land_on_the_home_page_home_page() {
+	}
+
+	@When("Admin gives the invalid LMS portal URL_HomePage")
+	public void admin_gives_the_invalid_lms_portal_url_home_page() {
+	}
+
+	@Then("Admin should receive {int} page not found error_HomePage")
+	public void admin_should_receive_page_not_found_error_home_page(Integer int1) {
+	}
+
+	@Then("HTTP response >= {int}. Then the link is broken_HomePage")
+	public void http_response_then_the_link_is_broken_home_page(Integer int1) {
+	}
+
+	@Then("Admin should see correct spellings in all fields_HomePage")
+	public void admin_should_see_correct_spellings_in_all_fields_home_page() {
+	}
+
+	@Then("Admin should see correct logo of the LMS_HomePage")
+	public void admin_should_see_correct_logo_of_the_lms_home_page() {
+	}
+
+	@Then("Admin should see  LMS - Learning Management System_HomePage")
+	public void admin_should_see_lms_learning_management_system_home_page() {
+	}
+
+	@Then("Admin should see company name below the app name_HomePage")
+	public void admin_should_see_company_name_below_the_app_name_home_page() {
+	}
+
+	@Then("Admin should see {string}_HomePage")
+	public void admin_should_see__home_page(String string) {
+	}
+
+	@Then("Admin should see two text field_HomePage")
+	public void admin_should_see_two_text_field_home_page() {
+	}
+
+	@Then("Admin should {string} in the first text field_HomePage")
+	public void admin_should_in_the_first_text_field_home_page(String string) {
+	}
+
+	@Then("Admin should see * symbol next to user text_HomePage")
+	public void admin_should_see_symbol_next_to_user_text_home_page() {
+	}
+
+	@Then("Admin should {string} in the second text field_HomePage")
+	public void admin_should_in_the_second_text_field_home_page(String string) {
+	}
+
+	@Then("Admin should see * symbol next to password text_HomePage")
+	public void admin_should_see_symbol_next_to_password_text_home_page() {
+	}
+
+	@Then("Admin should see input field on the centre of the page_HomePage")
+	public void admin_should_see_input_field_on_the_centre_of_the_page_home_page() {
+	}
+
+	@Then("Admin should see login button _HomePage")
+	public void admin_should_see_login_button__home_page() {
+	}
+
+	@Then("Admin should see login button on the centre of the page_HomePage")
+	public void admin_should_see_login_button_on_the_centre_of_the_page_home_page() {
+	}
+
+}
